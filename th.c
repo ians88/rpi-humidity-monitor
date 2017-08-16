33a4ee736c43609938d34698830e2357282299e2
Ian scheele <ian@salescare.nl>
2017-08-16 13:31:34 +0200

    test

--- th.c
+++ th.c
@@ -91,8 +91,8 @@ void dht11_read_val()
   }
 
   // verify cheksum and print the verified data
-  printf(dht11_val);
-  printf(j);
+  printf("Temp = %d",dht11_val[0]);
+  printf("J = %d",j);
 
   if((j>=40)&&(dht11_val[4]==((dht11_val[0]+dht11_val[1]+dht11_val[2]+dht11_val[3])& 0xFF)))
   {
